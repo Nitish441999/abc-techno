da375d65209e1831907206e3fd408ef4e81bda12
Nitish Kumar <nitish441999@gmail.com>
2024-09-20 12:38:10 +0530

    some change

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -15,8 +15,8 @@ function Footer() {
         <div className="container flex flex-col justify-between py-10 mx-auto space-y-8 lg:flex-row lg:space-y-0">
           <div className="lg:w-1/3">
             <a rel="noopener noreferrer" href="#" className="flex justify-center space-x-3 lg:justify-start">
-              <div className="flex items-center justify-center w-12 h-12 rounded-full dark:bg-violet-600">
-                <img src="https://i.imgur.com/jbqcnpj.png" alt="logo" className="w-16 h-12" />
+              <div className="flex items-center justify-center w-20 h-20 rounded-full ">
+                <img src="https://i.imgur.com/jbqcnpj.png" alt="logo" className="w-18 h-12" />
               </div>
               <span className="self-center text-2xl font-semibold">ABC Technologies</span>
             </a>
@@ -29,7 +29,7 @@ function Footer() {
           <div className="grid grid-cols-2 text-sm gap-x-3 gap-y-8 lg:w-2/3 sm:grid-cols-4">
             <div className="space-y-3">
               <h3 className="tracking-wide uppercase dark:text-gray-900 text-lg font-bold">Product</h3>
-              <ul className="space-y-1">
+              <ul className="space-y-1 text-[17px]">
                 <li className="group">
                   <a rel="noopener noreferrer" href="#" className="text-white group-hover:text-blue-600 transition flex gap-2  items-center">
                     <span className='text-black'><AiOutlineCaretRight /></span>Features
@@ -55,7 +55,7 @@ function Footer() {
             <div className="space-y-3">
               <h3 className="tracking-wide uppercase dark:text-gray-900 text-lg font-bold">Company</h3>
               <ul className="space-y-1">
-                <li className="group">
+                <li className="group text-[17px]">
                   <a rel="noopener noreferrer" href="#" className="text-white group-hover:text-blue-600 transition flex gap-2  items-center">
                   <span className='text-black'><AiOutlineCaretRight /></span> Privacy
                   </a>
@@ -70,7 +70,7 @@ function Footer() {
             <div className="space-y-3">
               <h3 className="uppercase dark:text-gray-900 text-lg font-bold">Developers</h3>
               <ul className="space-y-1">
-                <li className="group">
+                <li className="group text-[17px]">
                   <a rel="noopener noreferrer" href="#" className="text-white group-hover:text-blue-600 transition flex gap-2  items-center">
                   <span className='text-black'><AiOutlineCaretRight /></span> Public API
                   </a>
